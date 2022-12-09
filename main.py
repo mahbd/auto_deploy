e2186f0b6e2a4c417f7206d937c6723bdd192c10
Mahmudul Alam <mahmudula2000@gmail.com>
2022-12-09 09:18:09 +0600

    now pull git after commit

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app = FastAPI()
 git_rep_data = {
     "mahbd/auto_deploy": {
         "app_type": "fastapi",
+        "dir": "/home/mah/projects/auto_deploy",
         "pip_install": "/home/mah/projects/auto_deploy/venv/bin/pip3 install -r requirements.txt",
         "extra_commands": [],
         "restart": "sudo systemctl restart auto_deploy",
     },
     "mahbd/todo-api": {
         "app_type": "django",
+        "dir": "/home/mah/projects/todo-api",
         "pip_install": "/home/mah/projects/todo-api/venv/bin/pip3 install -r requirements.txt",
         "extra_commands": [
             "/home/mah/projects/todo-api/venv/bin/python3 manage.py migrate",
@@ -29,6 +31,8 @@ def read_root():
 
 
 def deploy_repo(repo_data):
+    os.chdir(repo_data["dir"])
+    os.system("git pull")
     if repo_data["app_type"] == "fastapi":
         os.system(repo_data["pip_install"])
         for command in repo_data["extra_commands"]:
@@ -47,7 +51,11 @@ def github_webhook(payload: dict):
     if repo_name in git_rep_data:
         repo_data = git_rep_data[repo_name]
         print("Deploying " + repo_name)
+        with open("log.txt", "a+") as f:
+            f.write("Deploying " + repo_name)
         threading.Thread(target=deploy_repo, args=(repo_data,)).start()
     else:
         print(f"Repo {repo_name} not found")
+        with open("log.txt", "a+") as f:
+            f.write(f"Repo {repo_name} not found")
     return {"response": True}
