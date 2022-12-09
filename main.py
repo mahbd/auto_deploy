fe09b2c7187807de38cb63b85911384d933eec79
Mahmudul Alam <mahmudula2000@gmail.com>
2022-12-09 09:08:08 +0600

    ready for fastapi and django

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
-import json
+import os
+import threading
 
 app = FastAPI()
+
+git_rep_data = {
+    "mahbd/auto_deploy": {
+        "app_type": "fastapi",
+        "pip_install": "/home/mah/projects/auto_deploy/venv/bin/pip3 install -r requirements.txt",
+        "extra_commands": [],
+        "restart": "sudo systemctl restart auto_deploy",
+    },
+    "mahbd/todo-api": {
+        "app_type": "django",
+        "pip_install": "/home/mah/projects/todo-api/venv/bin/pip3 install -r requirements.txt",
+        "extra_commands": [
+            "/home/mah/projects/todo-api/venv/bin/python3 manage.py migrate",
+            "/home/mah/projects/todo-api/venv/bin/python3 manage.py collectstatic --noinput",
+        ],
+        "restart": "sudo systemctl restart todo-api",
+    }
+}
 
 
 @app.get("/")
@@ -9,8 +28,26 @@ def read_root():
     return {"message": "Hello World"}
 
 
+def deploy_repo(repo_data):
+    if repo_data["app_type"] == "fastapi":
+        os.system(repo_data["pip_install"])
+        for command in repo_data["extra_commands"]:
+            os.system(command)
+        os.system(repo_data["restart"])
+    elif repo_data["app_type"] == "django":
+        os.system(repo_data["pip_install"])
+        for command in repo_data["extra_commands"]:
+            os.system(command)
+        os.system(repo_data["restart"])
+
+
 @app.post("/github/")
 def github_webhook(payload: dict):
-    json_string = json.dumps(payload)
-    print(json_string)
-    return {"message": "Hello World"}
+    repo_name = payload["repository"]["full_name"]
+    if repo_name in git_rep_data:
+        repo_data = git_rep_data[repo_name]
+        print("Deploying " + repo_name)
+        threading.Thread(target=deploy_repo, args=(repo_data,)).start()
+    else:
+        print(f"Repo {repo_name} not found")
+    return {"response": True}
